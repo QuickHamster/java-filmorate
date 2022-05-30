b2be9f0176fc2345c8b2fd55cbe0f26e5ec3f4e8
Alexander <spam.0ff@yandex.ru>
2022-05-30 15:15:38 +0300

    Init project

---
+++ src/test/java/ru/yandex/practicum/filmoreate/FilmoreateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmoreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmoreateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
